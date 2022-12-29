0aa4eed147668c03e65d8802781026ce65b8c40a
meriam <hamdaoui.meriam85@gmail.com>
2022-12-29 14:24:09 +0100

    file initiation

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./views/Home";
 import Dashboard from "./views/Dashboard";
 import MyLessons from "./views/MyLessons";
+import LogBar from "./components/bars/LogBar";
+import NavBar from "./components/bars/NavBar";
 
 const App = () => {
   return (
     <div className="App">
-      <h1>Hello eAlpha</h1>
+      <LogBar />
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="dashboard" element={<Dashboard />} />
--- src/components/bars/LogBar.jsx
+++ src/components/bars/LogBar.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { Nav } from "react-bootstrap";
+import "../../assets/css/Bars.css";
 
 const LogBar = () => {
-  return <div>LogBar</div>;
+  const [auth, setAuth] = React.useState(false);
+  return (
+    <div className="log-container">
+      <div className="log-logo">
+        <img
+          alt="Alpha Publishing"
+          width="153"
+          height="40"
+          data-src="https://www.alphapublishing.com/wp-content/themes/eduma/images/logo-sticky.png"
+          class=" ls-is-cached lazyloaded"
+          src="https://www.alphapublishing.com/wp-content/themes/eduma/images/logo-sticky.png"
+        />
+      </div>
+
+      <Nav className="log-menu">
+        {auth ? (
+          <>
+            <Nav.Link href="/">search</Nav.Link>
+            <Nav.Link href="/">user name</Nav.Link>
+            <Nav.Link href="/">logout</Nav.Link>
+          </>
+        ) : (
+          <>
+            <Nav.Link href="/" onClick={() => setAuth(true)}>
+              login
+            </Nav.Link>
+            <Nav.Link href="/">register</Nav.Link>
+          </>
+        )}
+      </Nav>
+    </div>
+  );
 };
 
 export default LogBar;

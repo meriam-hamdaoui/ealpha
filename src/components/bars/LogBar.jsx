3e5069ab8c34765cd175f43ce23cb190ccc54bca
meriam <hamdaoui.meriam85@gmail.com>
2022-12-30 12:15:24 +0100

    reprend

--- src/components/bars/LogBar.jsx
+++ src/components/bars/LogBar.jsx
@@ -1,38 +1,19 @@
 import React from "react";
-import { Nav } from "react-bootstrap";
 import "../../assets/css/Bars.css";
 
 const LogBar = () => {
-  const [auth, setAuth] = React.useState(false);
   return (
-    <div className="log-container">
-      <div className="log-logo">
+    <div className="header__first-row mat-toolbar mat-toolbar-single-row">
+      <div>
         <img
           alt="Alpha Publishing"
-          width="153"
-          height="40"
+          width="170"
+          height="50"
           data-src="https://www.alphapublishing.com/wp-content/themes/eduma/images/logo-sticky.png"
-          class=" ls-is-cached lazyloaded"
+          className="ls-is-cached lazyloaded"
           src="https://www.alphapublishing.com/wp-content/themes/eduma/images/logo-sticky.png"
         />
       </div>
-
-      <Nav className="log-menu">
-        {auth ? (
-          <>
-            <Nav.Link href="/">search</Nav.Link>
-            <Nav.Link href="/">user name</Nav.Link>
-            <Nav.Link href="/">logout</Nav.Link>
-          </>
-        ) : (
-          <>
-            <Nav.Link href="/" onClick={() => setAuth(true)}>
-              login
-            </Nav.Link>
-            <Nav.Link href="/">register</Nav.Link>
-          </>
-        )}
-      </Nav>
     </div>
   );
 };
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure magnam
+      vitae obcaecati laboriosam officia quam soluta necessitatibus aliquam
+      enim, aspernatur quaerat eos in rerum quasi, fugit expedita? Amet, atque
+      dolore! Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste cum
+      aliquid perferendis quibusdam, modi, nam est enim laborum quod nihil
+      pariatur aut distinctio totam eum quasi nulla officiis fuga perspiciatis.
+      <br />
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime natus
+      libero itaque repellat velit hic cumque veniam adipisci tempora
+      blanditiis, qui magnam, similique sunt eius? Laborum distinctio debitis
+      perspiciatis nihil. Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Nisi earum ut assumenda, sequi nam debitis corporis doloribus iste
+      ex voluptatibus consequuntur, ad voluptas quo? Nesciunt sapiente maiores
+      sit repudiandae saepe.
+      <br />
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam quibusdam
+      reprehenderit esse quaerat, incidunt facilis eaque pariatur a itaque enim
+      quos eum eos aliquid cupiditate. At iusto necessitatibus nisi aperiam?
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse, quos rem.
+      Quos consectetur, eius, fuga adipisci sint nobis aperiam omnis, sunt
+      laborum nesciunt obcaecati magni perferendis? Doloremque nobis laudantium
+      deleniti!
+    </div>
+  );
 };
 
 export default Home;
